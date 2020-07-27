89a5e9a70ffaf4aa88305b629c96e652baa357a1
Julian Marrades <julian.marrades@hotmail.es>
2020-07-27 16:01:24 +0200

    Added Twitch Icon

--- js/footer_functionality.js
+++ js/footer_functionality.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
-  for (const social of ['tw', 'in', 'git', 'yt']) {
+  for (const social of ['tw', 'in', 'git', 'yt', 'tv']) {
     let bgName = '#' + social + '-bg'
     let iconName = '#' + social + '-icon'
     $(bgName).hover(function() {
-      // console.log('Hovering over ' + bgName);
-      // console.log('Animating ' + iconName);
       $(iconName).animate({
         color: 'white'
       }, 100);
     }, function() {
-      // console.log('Leaving ' + bgName);
-      // console.log('Animating ' + iconName);
       $(iconName).animate({
         color: '#999'
       }, 100);
